c95b912445728ead0ff2568060e3a04286fd54e2
mesch-IT <meschackkapanga@gmail.com>
2022-05-14 11:40:48 +0100

    projet avec enregistrement sans modification

--- script.js
+++ script.js
@@ -3,43 +3,292 @@
 let valid = false
 
 
-
-const buttonCreer = document.getElementById("buttonCreer")
+let form = document.getElementsByClassName("grid")
+let buttonCreer = document.getElementById("buttonCreer")
 const buttonReini = document.getElementById("buttonReini")
 // const form = document.getElementsByTagName("form")
 
-//Réinitialiser
-buttonCreer.addEventListener("reset", function (event) {
-    
-          
+let selectField = document.getElementById("selectField")
+let selectText = document.getElementById("selectText")
+let options = document.getElementsByClassName("options")
+let list = document.getElementById("list")
+let iconArrow = document.getElementById("iconArrow")
+let image_input = document.getElementById("image")
+let uploaded_image = ""
 
-})
 
+// image_input.addEventListener("change", function () {
+
+//   const reader = new FileReader()
+//   reader.addEventListener("load", () => {
+//     uploaded_image = reader.result
+
+//     document.getElementById("displayImage").textContent  = `url(${uploaded_image})`
+//   })
+//   reader.readAsDataURL(this.files[0])
+// })
+let image = document.getElementById("displayImage")
+
+let lien = ""
+
+let source = ""
+
+
+let chargerImage = function (event) {
+  image.src = URL.createObjectURL(event.target.files[0])
+  // image.style.display = "block"
+  image.style.position = "relative"
+  image.style.top = "20px"
+  image.style.right = "-100px"
+  lien = URL.createObjectURL(event.target.files[0])
+
+}
+
+
+
+
+
+selectField.onclick = function () {
+  list.classList.toggle("hide")
+  iconArrow.classList.toggle("rotate")
+}
+
+for (option of options) {
+  option.onclick = function () {
+    selectText.innerHTML = this.textContent
+    list.classList.toggle("hide")
+    iconArrow.classList.toggle("rotate")
+
+  }
+}
+
+// //Réinitialiser
+// buttonReini.addEventListener("click", function (event) {
+
+//   document.getElementById("prenom").value = ""
+//   document.getElementById("nom").value  = ""
+//   document.getElementById("bio").value = ""
+//   document.getElementById("selectText").textContent = ""
+//   document.getElementById("displayImage").remove()
+
+
+// })
+const listContact = document.getElementById("contacts")
+
+
+
+let i = 0
 buttonCreer.addEventListener("click", function (event) {
-    const prenom = document.getElementById("prenom").value
-    const nom = document.getElementById("nom").value
-    const bio = document.getElementById("bio").value
-    const groupe = document.getElementById("groupe").value
-    const listContact = document.getElementById("contacts")
-    event.preventDefault()
+  event.preventDefault()
+ 
+  source = image_input.value
+  let divDisplay = document.createElement("div")
+  divDisplay.classList.add("contacts")
 
-    let listPrenom = document.createElement("div")
-    listPrenom.textContent = prenom
-    listContact.appendChild(listPrenom)
+  let grid2 = document.getElementById("grid2")
+  grid2.appendChild(divDisplay)
+  let textId = document.createElement("h6")
+  textId.textContent = ++i
+  console.log(i);
 
-    let listNom = document.createElement("div")
-    listNom.textContent = nom
-    listContact.appendChild(listNom)
 
-    let listGroupe = document.createElement("div")
-    listGroupe.textContent = groupe
-    listContact.appendChild(listGroupe)
+  let prenom = document.getElementById("prenom").value
+  let nom = document.getElementById("nom").value
+  let bio = document.getElementById("bio").value
+  let selectText = document.getElementById("selectText").textContent
+  let deleteItem = document.createElement("img")
+  let createImage = document.createElement("img")
+  createImage.src = lien
+  createImage.classList.add("showImage")
 
-    let listBio = document.createElement("div")
-    listBio.textContent = bio
-    listContact.appendChild(listBio)
+  divDisplay.appendChild(createImage)
+
+  deleteItem.src = "delete.png"
+  deleteItem.classList.add("delete")
+
+  divDisplay.appendChild(deleteItem)
+  // let showImage = document.createElement("img")
+  // showImage.src =  `url(${uploaded_image})`
+
+
+
+  let listPrenom = document.createElement("span")
+  listPrenom.classList.add("userData")
+  listPrenom.textContent = prenom
+  divDisplay.appendChild(listPrenom)
+
+  let listNom = document.createElement("span")
+  listNom.classList.add("userData")
+  listNom.textContent = nom
+  divDisplay.appendChild(listNom)
+
+  let listText = document.createElement("h4")
+  listText.textContent = selectText
+  divDisplay.appendChild(listText)
+
+  let listBio = document.createElement("p")
+  listBio.classList.add("describeBio")
+  listBio.textContent = bio
+  divDisplay.appendChild(listBio)
+
+    divDisplay.appendChild(textId)
+
+  
+
+  // document.getElementById("hr").style.visibility = "visible"
+
+  deleteItem.addEventListener("click", function () {
+    // listPrenom.remove()
+    // listNom.remove()
+    // listBio.remove()
+    // listText.remove()
+    // createImage.remove()
+
+    divDisplay.remove()
+
+    deleteItem.style.display = "none"
+
+
+  })
+   let id = ""
+  divDisplay.addEventListener("click", function () {
+
+          let id = textId.textContent
+         console.log(id);
+
+          console.log("textid " +textId.textContent);
+
+          
+ 
+    prenom = divDisplay.childNodes[2]
+    document.getElementById("prenom").value =  textId.textContent
+
+    nom = divDisplay.childNodes[3]
+    document.getElementById("nom").value = nom.textContent
+
+    bio = divDisplay.childNodes[5]
+    document.getElementById("bio").value = bio.textContent
+
+    selectText = divDisplay.childNodes[4]
+
+    document.getElementById("selectText").textContent = selectText.textContent
+
+
+     
+    //  image_input.textContent = source
+    // console.log(divDisplay.childNodes[0]);
+     
+    //  document.getElementById("displayImage").src =  divDisplay.childNodes[0]
+
+         
+       image.src =   divDisplay.childNodes[0].src
+
+            console.log(image.src);
+    
+        //  image.src = lien
+    //  image.style.display = "block"
+      // image.style.visibility = "visible"
+     
+
+
+
+
+
+    document.getElementById("prenom").addEventListener("change",function(){
+
+
+      divDisplay.childNodes[2].textContent = document.getElementById("prenom").value
+
+              
+      console.log(textId.textContent+ " etb "+ id);
+
+     
+    })
+    document.getElementById("nom").addEventListener("change",function(){
+      console.log(textId);
+      divDisplay.childNodes[3].textContent = document.getElementById("nom").value
+     
+    })
+    document.getElementById("bio").addEventListener("change",function(){
+      divDisplay.childNodes[5].textContent = document.getElementById("bio").value
+     
+    })
+    for (option of options) {
+      option.onclick = function () {
+        document.getElementById("selectText").textContent = this.textContent
+        divDisplay.childNodes[4].textContent = document.getElementById("selectText").textContent
+        list.classList.toggle("hide")
+    iconArrow.classList.toggle("rotate")
+       
+    
+      }
+    }
+
+        // buttonCreer.addEventListener("click",function(){
+
+   
+        // })
+    // document.getElementsByClassName("options").addEventListener("change",function(){
+    //   // divDisplay.childNodes[4].textContent = document.getElementById("selectText").textContent
+    //   // console.log(document.getElementById("selectText").textContent);
+      
+     
+    // })
+
+   
+
+
+
+    //  document.getElementById("prenom").addEventListener("change",function (){
+    //       f
+    //   })
+
+
+
+
+
+
+
+
+
+
+
+
+    // document.getElementById("displayImage").value = image.textContent
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
+
+})
+
+let grid2 = document.getElementById("grid2")
+
+//Réinitialiser
+buttonReini.addEventListener("click", function (event) {
+
+  document.getElementById("prenom").value = ""
+  document.getElementById("nom").value = ""
+  document.getElementById("bio").value = ""
+  document.getElementById("selectText").textContent = ""
+  // document.getElementById("displayImage").style.visibility = "hidden"
+
+
+})
+
+
+
 
 
 
